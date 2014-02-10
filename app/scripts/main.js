e3afb50f079ca05ff5ad5f20734ab7b4acce631c
yellow10 <yellow.10@qq.com>
2014-02-10 20:07:19 +0800

    snap js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,3 +3,8 @@ console.log('\'Allo \'Allo!');
 $(document).ready(function() {
 	$('.typekit-badge').hide();
 });
+
+var snapper = new Snap({
+                element: document.getElementById('content'),
+                disable: 'right'
+            });   

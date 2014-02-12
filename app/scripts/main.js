c4b80b2f4f650ce0b38cd73fbb595b11e93a002c
yellow10 <yellow.10@qq.com>
2014-02-12 14:51:01 +0800

    home finished

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,9 +2,40 @@ console.log('\'Allo \'Allo!');
 
 $(document).ready(function() {
 	$('.typekit-badge').hide();
+
+	/*sidebar decs.*/
+	$('.works ul li a').mouseenter(function() {
+		$(this).siblings('p').stop().show(200)
+	});
+
+	$('.works ul li a').mouseleave(function() {
+		$(this).siblings('p').stop().hide(200)
+	});
 });
 
+/* sidebar */
 var snapper = new Snap({
-                element: document.getElementById('content'),
-                disable: 'right'
-            });   
+    element: document.getElementById('content'),
+    disable: 'right',
+    transitionSpeed: 0.2
+});   
+
+$('.menu').click(function() {
+    snapper.open('left');
+    $('.menu').hide();
+});
+
+$('.snap-content').click(function() {
+	if( snapper.state().state=="left" ){
+        snapper.close();
+        $('.menu').delay(200).show(0);
+    }
+});
+
+$('.close-snap').click(function() {
+	if( snapper.state().state=="left" ){
+        snapper.close();
+        $('.menu').delay(200).show(0);
+    }
+});
+

318c868655205f6c1e145f09247596495944cca0
yellow10 <yellow.10@qq.com>
2014-02-18 14:44:08 +0800

    doing rim page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,12 +5,26 @@ $(document).ready(function() {
 
 	/*sidebar decs.*/
 	$('.works ul li a').mouseenter(function() {
-		$(this).siblings('p').stop().show(200)
+		$(this).siblings('p').stop().show(200);
 	});
 
 	$('.works ul li a').mouseleave(function() {
-		$(this).siblings('p').stop().hide(200)
+		$(this).siblings('p').stop().hide(200);
 	});
+
+    var mySwipe = Swipe(document.getElementById('slider'), {
+        speed: 400,
+        auto: 4000,
+        continuous: true,
+    });
+
+    $('.page-slider .prev').click(function() {
+        mySwipe.prev();
+    });
+
+    $('.page-slider .next').click(function() {
+        mySwipe.next();
+    });
 });
 
 /* sidebar */
@@ -26,14 +40,14 @@ $('.menu').click(function() {
 });
 
 $('.snap-content').click(function() {
-	if( snapper.state().state=="left" ){
+	if( snapper.state().state === "left" ){
         snapper.close();
         $('.menu').delay(200).show(0);
     }
 });
 
 $('.close-snap').click(function() {
-	if( snapper.state().state=="left" ){
+	if( snapper.state().state === "left" ){
         snapper.close();
         $('.menu').delay(200).show(0);
     }
